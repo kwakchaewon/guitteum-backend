f487beae9cf30121812fb84c044d46d0ec2730b7
kwakchaewon <ksh03003@naver.com>
2026-02-13 15:20:11 +0900

    fix: SpeechItemReader 빈 이름 충돌 해소

--- src/main/java/com/guitteum/batch/reader/SpeechItemReaderConfig.java
+++ src/main/java/com/guitteum/batch/reader/SpeechItemReaderConfig.java
@@ -6,14 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.Sort;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
 
 import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
-public class SpeechItemReader {
+public class SpeechItemReaderConfig {
 
     private final SpeechRepository speechRepository;
 
